0ce84b6f008bbbd5821bc9db00d3b321401b7436
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-09 23:55:14 +0200

    ch07: add project from chapter06

---
+++ chapter07-measuring-coverage-with-coverage-py/src/random_wikipedia_article/__init__.py
@@ -0,0 +1,38 @@
+import sys
+from dataclasses import dataclass
+from importlib.metadata import metadata
+
+import httpx
+from rich.console import Console
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+USER_AGENT = "{Name}/{Version} (Contact: {Author-email})"
+
+
+@dataclass
+class Article:
+    title: str = ""
+    summary: str = ""
+
+
+def fetch(url):
+    fields = metadata("random-wikipedia-article")
+    headers = {"User-Agent": USER_AGENT.format_map(fields)}
+
+    with httpx.Client(headers=headers, http2=True) as client:
+        response = client.get(url, follow_redirects=True)
+        response.raise_for_status()
+        data = response.json()
+
+    return Article(data["title"], data["extract"])
+
+
+def show(article, file):
+    console = Console(file=file, width=72, highlight=False)
+    console.print(article.title, style="bold", end="\n\n")
+    console.print(article.summary)
+
+
+def main():
+    article = fetch(API_URL)
+    show(article, sys.stdout)
---
+++ chapter07-measuring-coverage-with-coverage-py/tests/test_main.py
@@ -0,0 +1,53 @@
+import io
+import subprocess
+import sys
+
+import pytest
+from factory import Factory, Faker
+
+from random_wikipedia_article import Article, fetch, show
+
+
+def test_output():
+    args = [sys.executable, "-m", "random_wikipedia_article"]
+    process = subprocess.run(args, capture_output=True, check=True)
+    assert process.stdout
+
+
+@pytest.fixture
+def file():
+    return io.StringIO()
+
+
+def parametrized_fixture(*params):
+    return pytest.fixture(params=params)(lambda request: request.param)
+
+
+class ArticleFactory(Factory):
+    class Meta:
+        model = Article
+
+    title = Faker("sentence")
+    summary = Faker("paragraph")
+
+
+article = parametrized_fixture(*ArticleFactory.build_batch(10))
+
+
+def test_final_newline(article, file):
+    show(article, file)
+    assert file.getvalue().endswith("\n")
+
+
+@pytest.fixture
+def serve(httpserver):
+    def f(article):
+        json = {"title": article.title, "extract": article.summary}
+        httpserver.expect_request("/").respond_with_json(json)
+        return httpserver.url_for("/")
+
+    return f
+
+
+def test_fetch(article, serve):
+    assert article == fetch(serve(article))
