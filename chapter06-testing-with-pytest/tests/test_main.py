fff3d73948ff87b99228df778b28c6bfcee6a92c
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-09 22:38:07 +0200

    ch06: run test against multiple articles with parameterization

--- chapter06-testing-with-pytest/tests/test_main.py
+++ chapter06-testing-with-pytest/tests/test_main.py
@@ -18,7 +18,18 @@ def file():
     return io.StringIO()
 
 
-def test_final_newline(file):
-    article = Article("Lorem Ipsum", "Lorem ipsum dolor sit amet.")
+articles = [
+    Article(),
+    Article("test"),
+    Article("Lorem Ipsum", "Lorem ipsum dolor sit amet."),
+    Article(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "Nulla mattis volutpat sapien, at dapibus ipsum accumsan eu.",
+    ),
+]
+
+
+@pytest.mark.parametrize("article", articles)
+def test_final_newline(article, file):
     show(article, file)
     assert file.getvalue().endswith("\n")
