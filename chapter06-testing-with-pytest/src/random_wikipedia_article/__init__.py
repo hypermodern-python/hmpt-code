76445ef610a9465a0f3210bb3f4e14ea9fbc4aa0
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-09 23:05:16 +0200

    ch06: use httpx to implement the fetch function

--- chapter06-testing-with-pytest/src/random_wikipedia_article/__init__.py
+++ chapter06-testing-with-pytest/src/random_wikipedia_article/__init__.py
@@ -1,11 +1,12 @@
 import sys
-import json
-import urllib.request
 from dataclasses import dataclass
+from importlib.metadata import metadata
 
+import httpx
 from rich.console import Console
 
 API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+USER_AGENT = "{Name}/{Version} (Contact: {Author-email})"
 
 
 @dataclass
@@ -15,8 +16,14 @@ class Article:
 
 
 def fetch(url):
-    with urllib.request.urlopen(url) as response:
-        data = json.load(response)
+    fields = metadata("random-wikipedia-article")
+    headers = {"User-Agent": USER_AGENT.format_map(fields)}
+
+    with httpx.Client(headers=headers, http2=True) as client:
+        response = client.get(url, follow_redirects=True)
+        response.raise_for_status()
+        data = response.json()
+
     return Article(data["title"], data["extract"])
 
 
