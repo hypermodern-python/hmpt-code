e70c1494e9834d76bf9489a50b0c355cf7a718b2
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-09 20:04:51 +0200

    ch06: add test for the show function

--- chapter06-testing-with-pytest/tests/test_main.py
+++ chapter06-testing-with-pytest/tests/test_main.py
@@ -1,8 +1,18 @@
+import io
 import subprocess
 import sys
+
+from random_wikipedia_article import Article, show
 
 
 def test_output():
     args = [sys.executable, "-m", "random_wikipedia_article"]
     process = subprocess.run(args, capture_output=True, check=True)
     assert process.stdout
+
+
+def test_final_newline():
+    article = Article("Lorem Ipsum", "Lorem ipsum dolor sit amet.")
+    file = io.StringIO()
+    show(article, file)
+    assert file.getvalue().endswith("\n")
