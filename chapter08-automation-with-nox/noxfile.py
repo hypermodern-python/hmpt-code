2912e93bbea71c2fb05ea83eedae427e463d941d
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-10 17:19:25 +0200

    ch08: add nox session for building packages

--- chapter08-automation-with-nox/noxfile.py
+++ chapter08-automation-with-nox/noxfile.py
@@ -1,4 +1,19 @@
+import shutil
+from pathlib import Path
+
 import nox
+
+
+@nox.session
+def build(session):
+    session.install("build", "twine")
+
+    distdir = Path("dist")
+    if distdir.exists():
+        shutil.rmtree(distdir)
+
+    session.run("python", "-m", "build")
+    session.run("twine", "check", *distdir.glob("*"))
 
 
 @nox.session
