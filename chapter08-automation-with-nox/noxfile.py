0a77cdf58848c5f6ec28aeacaebd12a96eada95a
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-10 18:08:42 +0200

    ch08: add nox session for coverage report

--- chapter08-automation-with-nox/noxfile.py
+++ chapter08-automation-with-nox/noxfile.py
@@ -1,6 +1,7 @@
 """
 Run the checks and tasks for this project.
 """
+
 import shutil
 from pathlib import Path
 
@@ -28,3 +29,11 @@ def tests(session):
     """Run the test suite."""
     session.install(".[tests]")
     session.run("coverage", "run", "-m", "pytest", *session.posargs)
+
+
+@nox.session
+def coverage(session):
+    session.install("coverage[toml]")
+    if any(Path().glob(".coverage.*")):
+        session.run("coverage", "combine")
+    session.run("coverage", "report")
