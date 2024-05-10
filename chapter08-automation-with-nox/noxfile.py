7a4ebd71a3149a1d71bb4bd40d1a635a00a86b46
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-10 17:35:16 +0200

    ch08: add docstrings for nox sessions

--- chapter08-automation-with-nox/noxfile.py
+++ chapter08-automation-with-nox/noxfile.py
@@ -1,3 +1,6 @@
+"""
+Run the checks and tasks for this project.
+"""
 from pathlib import Path
 
 import nox
@@ -7,6 +10,7 @@ nox.options.error_on_external_run = True
 
 @nox.session
 def build(session):
+    """Build the package."""
     session.install("twine")
     session.run("poetry", "build", external=True)
     session.run("twine", "check", *Path().glob("dist/*"))
@@ -14,5 +18,6 @@ def build(session):
 
 @nox.session
 def tests(session):
+    """Run the test suite."""
     session.install(".", "pytest", "pytest-httpserver", "factory-boy")
     session.run("pytest")
