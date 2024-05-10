cb069b05d2a03e1abdb944c900d16dc42831c0dc
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-10 18:10:51 +0200

    ch08: add python 3.7+ to the tests session

--- chapter08-automation-with-nox/noxfile.py
+++ chapter08-automation-with-nox/noxfile.py
@@ -24,7 +24,7 @@ def build(session):
     session.run("twine", "check", *distdir.glob("*"))
 
 
-@nox.session(python=["3.12", "3.11", "3.10"])
+@nox.session(python=["3.12", "3.11", "3.10", "3.9", "3.8", "3.7"])
 def tests(session):
     """Run the test suite."""
     session.install(".[tests]")
