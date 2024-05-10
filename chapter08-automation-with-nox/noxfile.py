c85656701eb97f738f6a29b4df30267754f22c5c
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-10 17:15:29 +0200

    ch08: install dev-requirements.txt in tests session

--- chapter08-automation-with-nox/noxfile.py
+++ chapter08-automation-with-nox/noxfile.py
@@ -3,5 +3,6 @@ import nox
 
 @nox.session
 def tests(session):
-    session.install(".[tests]")
+    session.install("-r", "dev-requirements.txt")
+    session.install(".", "--no-deps")
     session.run("pytest")
