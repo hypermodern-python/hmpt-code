9d22220b6b58dbc049b38d99ea0876e657ace0df
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-11 17:36:56 +0200

    ch09: reformat with autopep8

--- chapter09-linting-with-ruff-and-pre-commit/example.py
+++ chapter09-linting-with-ruff-and-pre-commit/example.py
@@ -1,8 +1,10 @@
 def create_frobnicator_factory(the_factory_name,
-                                  interval_in_secs=100,  dbg=False,
-                                use_singleton=None,frobnicate_factor=4.5):
-  if dbg:print('creating frobnication factory '+the_factory_name+"...")
-  if(use_singleton):   return _frob_sngltn      #we're done
-  return FrobnicationFactory( the_factory_name,
+                               interval_in_secs=100,  dbg=False,
+                               use_singleton=None, frobnicate_factor=4.5):
+    if dbg:
+        print('creating frobnication factory '+the_factory_name+"...")
+    if (use_singleton):
+        return _frob_sngltn  # we're done
+    return FrobnicationFactory(the_factory_name,
 
-    intrvl = interval_in_secs             ,f=frobnicate_factor     )
+                               intrvl=interval_in_secs, f=frobnicate_factor)
