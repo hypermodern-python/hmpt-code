8ae4064741faf381c4c1580015d05a5424cda83c
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-11 16:13:53 +0200

    ch09: fix mutable-argument-default (B006)

--- chapter09-linting-with-ruff-and-pre-commit/bad.py
+++ chapter09-linting-with-ruff-and-pre-commit/bad.py
@@ -1,7 +1,9 @@
 import subprocess
 
 
-def run(command, args=[], force=False):
+def run(command, args=None, force=False):
+    if args is None:
+        args = []
     if force:
         args.insert(0, "--force")
     subprocess.run([command, *args])
