d9fbabb7b26a886e52eb406e86ef77e90a1815a2
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-11 22:47:40 +0200

    ch10: add @dataclass decorator: implement decorator

--- chapter10-using-types-for-safety-and-inspection/dataclasses2.py
+++ chapter10-using-types-for-safety-and-inspection/dataclasses2.py
@@ -1,4 +1,6 @@
 import inspect
+import sys
+from typing import dataclass_transform
 
 
 def build_dataclass_init[T](cls: type[T]) -> str:
@@ -15,3 +17,15 @@ def build_dataclass_init[T](cls: type[T]) -> str:
         ", ".join(args),
         "\n".join(body),
     )
+
+
+@dataclass_transform()
+def dataclass[T](cls: type[T]) -> type[T]:
+    sourcecode = build_dataclass_init(cls)
+
+    globals = sys.modules[cls.__module__].__dict__
+    locals = {}
+    exec(sourcecode, globals, locals)
+
+    cls.__init__ = locals["__init__"]
+    return cls
