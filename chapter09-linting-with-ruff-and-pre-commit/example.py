dedabe4dd41e8128c9bd42a4096ef13917d3433a
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-11 17:38:40 +0200

    ch09: reformat with black

--- chapter09-linting-with-ruff-and-pre-commit/example.py
+++ chapter09-linting-with-ruff-and-pre-commit/example.py
@@ -1,10 +1,14 @@
-def create_frobnicator_factory(the_factory_name,
-                               interval_in_secs=100,
-                               dbg=False,
-                               use_singleton=None,
-                               frobnicate_factor=4.5):
-    if dbg: print('creating frobnication factory ' + the_factory_name + "...")
-    if (use_singleton): return _frob_sngltn  #we're done
-    return FrobnicationFactory(the_factory_name,
-                               intrvl=interval_in_secs,
-                               f=frobnicate_factor)
+def create_frobnicator_factory(
+    the_factory_name,
+    interval_in_secs=100,
+    dbg=False,
+    use_singleton=None,
+    frobnicate_factor=4.5,
+):
+    if dbg:
+        print("creating frobnication factory " + the_factory_name + "...")
+    if use_singleton:
+        return _frob_sngltn  # we're done
+    return FrobnicationFactory(
+        the_factory_name, intrvl=interval_in_secs, f=frobnicate_factor
+    )
