435a86d1b99c531b328d23d83d54bbe217ca2dc0
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-11 23:41:20 +0200

    ch10: add example for @typechecked

--- chapter10-using-types-for-safety-and-inspection/person.py
+++ chapter10-using-types-for-safety-and-inspection/person.py
@@ -1,6 +1,8 @@
+import json
+from pathlib import Path
 from typing import Any, TypedDict
 
-from typeguard import check_type
+from typeguard import check_type, typechecked
 
 
 class Person(TypedDict):
@@ -10,3 +12,9 @@ class Person(TypedDict):
     @classmethod
     def check(cls, data: Any) -> "Person":
         return check_type(data, Person)
+
+
+@typechecked
+def load_people(path: Path) -> list[Person]:
+    with path.open() as io:
+        return json.load(io)
