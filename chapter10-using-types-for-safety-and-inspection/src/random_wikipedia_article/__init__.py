5922e86f1c8d06eff667a088f285885c76df8839
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-11 21:33:12 +0200

    ch10: add type annotations to the project

--- chapter10-using-types-for-safety-and-inspection/src/random_wikipedia_article/__init__.py
+++ chapter10-using-types-for-safety-and-inspection/src/random_wikipedia_article/__init__.py
@@ -1,10 +1,11 @@
-import sys
 import json
+import sys
 import textwrap
 import urllib.request
 from dataclasses import dataclass
+from typing import Final, TextIO
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL: Final = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @dataclass
@@ -13,17 +14,17 @@ class Article:
     summary: str = ""
 
 
-def fetch(url):
+def fetch(url: str) -> Article:
     with urllib.request.urlopen(url) as response:
         data = json.load(response)
     return Article(data["title"], data["extract"])
 
 
-def show(article, file):
+def show(article: Article, file: TextIO) -> None:
     summary = textwrap.fill(article.summary)
     file.write(f"{article.title}\n\n{summary}\n")
 
 
-def main():
+def main() -> None:
     article = fetch(API_URL)
     show(article, sys.stdout)
