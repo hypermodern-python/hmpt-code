7c1b1ba7b7b3d9a06dbe8c63cfaa0feee8e5ba7b
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-19 13:44:03 +0200

    ch04: use rich to enhance console output

--- chapter04-dependency-management/src/random_wikipedia_article/__init__.py
+++ chapter04-dependency-management/src/random_wikipedia_article/__init__.py
@@ -1,7 +1,5 @@
-import textwrap
-
 import httpx
-
+from rich.console import Console
 
 API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 USER_AGENT = "random-wikipedia-article/0.1 (Contact: you@example.com)"
@@ -15,5 +13,6 @@ def main():
         response.raise_for_status()
         data = response.json()
 
-    print(data["title"], end="\n\n")
-    print(textwrap.fill(data["extract"]))
+    console = Console(width=72, highlight=False)
+    console.print(data["title"], style="bold", end="\n\n")
+    console.print(data["extract"])
