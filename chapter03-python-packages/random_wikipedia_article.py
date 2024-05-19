a8c049be2bf413729ae995ef1d6b9d7cb658335a
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-19 12:59:37 +0200

    ch03: add script random_wikipedia_article.py

---
+++ chapter03-python-packages/random_wikipedia_article.py
@@ -0,0 +1,17 @@
+import json
+import textwrap
+import urllib.request
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+
+def main():
+    with urllib.request.urlopen(API_URL) as response:
+        data = json.load(response)
+
+    print(data["title"], end="\n\n")
+    print(textwrap.fill(data["extract"]))
+
+
+if __name__ == "__main__":
+    main()
