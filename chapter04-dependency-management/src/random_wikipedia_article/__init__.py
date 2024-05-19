6f86bd05fff3079eb2da20022d1db9fd7a215fc4
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-19 13:42:53 +0200

    ch04: use httpx to consume the Wikipedia API

--- chapter04-dependency-management/src/random_wikipedia_article/__init__.py
+++ chapter04-dependency-management/src/random_wikipedia_article/__init__.py
@@ -1,13 +1,19 @@
-import json
 import textwrap
-import urllib.request
+
+import httpx
+
 
 API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+USER_AGENT = "random-wikipedia-article/0.1 (Contact: you@example.com)"
 
 
 def main():
-    with urllib.request.urlopen(API_URL) as response:
-        data = json.load(response)
+    headers = {"User-Agent": USER_AGENT}
+
+    with httpx.Client(headers=headers) as client:
+        response = client.get(API_URL, follow_redirects=True)
+        response.raise_for_status()
+        data = response.json()
 
     print(data["title"], end="\n\n")
     print(textwrap.fill(data["extract"]))
