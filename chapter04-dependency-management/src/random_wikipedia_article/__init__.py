74aa1b0574137e1a1a51c8854e5b63ce6d85fa77
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-05-19 13:48:40 +0200

    ch04: build user-agent header from metadata

--- chapter04-dependency-management/src/random_wikipedia_article/__init__.py
+++ chapter04-dependency-management/src/random_wikipedia_article/__init__.py
@@ -1,12 +1,20 @@
+from importlib.metadata import metadata
+
 import httpx
 from rich.console import Console
 
+
 API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
-USER_AGENT = "random-wikipedia-article/0.1 (Contact: you@example.com)"
+USER_AGENT = "{Name}/{Version} (Contact: {Author-email})"
+
+
+def build_user_agent():
+    fields = metadata("random-wikipedia-article")
+    return USER_AGENT.format_map(fields)
 
 
 def main():
-    headers = {"User-Agent": USER_AGENT}
+    headers = {"User-Agent": build_user_agent()}
 
     with httpx.Client(headers=headers, http2=True) as client:
         response = client.get(API_URL, follow_redirects=True)
