cf5adb0656e98b19ced7836fc4ab266e1cde0d5f
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-06-16 20:24:35 +0200

    ch08: parameterize tests by valid python and django combinations

--- chapter08-automation-with-nox/noxfile.py
+++ chapter08-automation-with-nox/noxfile.py
@@ -2,6 +2,15 @@ import nox
 
 
 @nox.session
-@nox.parametrize(["a", "b"], [("1.0", "2.2"), ("0.9", "2.1")])
-def tests(session, a, b):
-    print(a, b)  # only the combinations listed above
+@nox.parametrize(
+    ["python", "django"],
+    [
+        (python, django)
+        for python in ["3.12", "3.11", "3.10"]
+        for django in ["3.2.*", "4.2.*"]
+        if (python, django) not in [("3.12", "3.2.*"), ("3.11", "3.2.*")]
+    ],
+)
+def tests(session, django):
+    session.install(".", "pytest-django", f"django=={django}")
+    session.run("pytest")
