4ebf263567a8300a851bf3f45c470835b51af443
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-06-16 13:31:46 +0200

    ch05: support python 3.7 with importlib-metadata

--- chapter05-managing-projects-with-poetry/src/random_wikipedia_article/__init__.py
+++ chapter05-managing-projects-with-poetry/src/random_wikipedia_article/__init__.py
@@ -1,7 +1,12 @@
+import sys
+
 import httpx
 from rich.console import Console
 
-from importlib.metadata import metadata
+if sys.version_info >= (3, 8):
+    from importlib.metadata import metadata
+else:
+    from importlib_metadata import metadata
 
 API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 USER_AGENT = "{Name}/{Version} (Contact: {Author-email})"
