471627b6e316d98a089e175b604fd91d22e8c762
Claudio Jolowicz <mail@claudiojolowicz.com>
2024-06-16 19:15:06 +0200

    ch08: add session for building with poetry

--- chapter08-automation-with-nox/noxfile.py
+++ chapter08-automation-with-nox/noxfile.py
@@ -1,6 +1,15 @@
+from pathlib import Path
+
 import nox
 
 nox.options.error_on_external_run = True
+
+
+@nox.session
+def build(session):
+    session.install("twine")
+    session.run("poetry", "build", external=True)
+    session.run("twine", "check", *Path().glob("dist/*"))
 
 
 @nox.session
